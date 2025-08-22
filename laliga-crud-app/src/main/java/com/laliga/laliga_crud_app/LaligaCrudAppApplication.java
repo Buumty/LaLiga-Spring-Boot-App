d8cfbf96479cee2e0fca7618ae5f8f8e194ba7d9
Buumty <wojtekand22@gmail.com>
2025-08-22 18:59:43 +0200

    Creating Spring Boot project

---
+++ laliga-crud-app/src/main/java/com/laliga/laliga_crud_app/LaligaCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.laliga.laliga_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaligaCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaligaCrudAppApplication.class, args);
+	}
+
+}
---
+++ laliga-crud-app/src/test/java/com/laliga/laliga_crud_app/LaligaCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laliga.laliga_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaligaCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

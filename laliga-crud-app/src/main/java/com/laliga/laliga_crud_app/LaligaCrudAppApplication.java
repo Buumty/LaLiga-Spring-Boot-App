62cc1b0edfc2522204619281302d9316c32c83d1
Buumty <wojtekand22@gmail.com>
2025-08-25 20:51:20 +0200

    Creating controller

--- laliga-crud-app/src/main/java/com/laliga/laliga_crud_app/LaligaCrudAppApplication.java
+++ laliga-crud-app/src/main/java/com/laliga/laliga_crud_app/LaligaCrudAppApplication.java
@@ -1,10 +1,14 @@
 package com.laliga.laliga_crud_app;
 
+import com.laliga.laliga_crud_app.player.Player;
 import com.laliga.laliga_crud_app.player.PlayerRepository;
+import com.laliga.laliga_crud_app.player.PlayerService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class LaligaCrudAppApplication {
@@ -13,7 +17,7 @@ public class LaligaCrudAppApplication {
 		SpringApplication.run(LaligaCrudAppApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner run(PlayerRepository playerRepository) {
+	public CommandLineRunner run(PlayerRepository playerRepository, PlayerService playerService) {
 		return args -> {
 			long count = playerRepository.count();
 			System.out.println("📊 Liczba rekordów w tabeli player_data: " + count);
